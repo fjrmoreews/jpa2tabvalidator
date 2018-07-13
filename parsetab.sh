d76a4b26793e276fc2b80e8c78d4dfed2f0b9a8d
MOREEWS Francois <francois.moreews@irisa.fr>
2018-07-13 17:19:08 +0200

    maven build migration

--- parsetab.sh
+++ parsetab.sh
@@ -10,7 +10,10 @@ CPL=bin:$LIB
 
 ENTRYPOINT=tabvalidator.ValidateEntityFromFile
 
-PARAM=" -n example/tab/lipmuscl.xlsx -t oo -g example/jpa_generated_test1.jar"
+#PARAM=" -n example/tab/lipmuscl.xlsx -t oo -g example/jpa_generated_test2.jar"
+
+
+PARAM=" -n example/tab/baseanalyseDateIssue.xlsx -t oo -g example/generated_model.jar"
 
 echo $JAVA $OPT -classpath $CPL $ENTRYPOINT $PARAM
 $JAVA $OPT -classpath $CPL $ENTRYPOINT $PARAM
---
+++ run_getjarlib.sh
@@ -0,0 +1,9 @@
+rm -rf ./lib
+mvn -Dhttps.protocols=TLSv1.2  clean dependency:copy-dependencies
+
+mkdir -p target/dependency
+mkdir -p ./lib
+mv target/dependency/* ./lib/
+
+
+
